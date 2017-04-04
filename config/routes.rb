132c1333371bc8049f0c08777560f94ef2c530f7
Truong Ngoc Tuan <truongtuan97@gmail.com>
2017-04-04 13:14:00 +0700

    Create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'static_pages#index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+  root :to => "static_pages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

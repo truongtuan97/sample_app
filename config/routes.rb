49fe5eba399ddc62105c06cda749418f84df15b7
Truong Ngoc Tuan <truongtuan97@gmail.com>
2017-04-04 15:16:11 +0700

    Create login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
   root :to => "static_pages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
